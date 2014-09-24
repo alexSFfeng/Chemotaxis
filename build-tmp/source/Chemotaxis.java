67ee8db65ba31278073d826f63b6cbdf0fd57d3f
unknown <shfeng1@697-334-Theoden.sf.k12.internal>
2014-09-24 10:05:28 -0700

    submitting Chemotaxis assignment

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,24 @@
-  Bacteria [] colony;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
+ //declare bacteria variables here   
+ Bacteria [] colony;
  Food energy;
  Food toxin;
- void setup()   
+ public void setup()   
  {     
  	size(600,600);
  	colony = new Bacteria[200];
@@ -12,7 +29,7 @@
  	energy = new Food();
  	toxin  = new Food();
  }   
- void draw()   
+ public void draw()   
  {    
  	background(0); 
     for(int i = 0;i < colony.length; i++)
@@ -36,7 +53,7 @@
  		bacteriaG = (int)(Math.random()*150) + 50;
  		bacteriaB = (int)(Math.random()*150) + 50;
  	} 
- 	void move()
+ 	public void move()
  	{
 
  	  bacteriaX = bacteriaX + (int)(Math.random()*9) - 4;
@@ -76,7 +93,7 @@
       }
     
  	}
- 	void show()
+ 	public void show()
  	{
      strokeWeight(2);
      stroke(bacteriaR,bacteriaG,bacteriaB);
@@ -94,7 +111,7 @@
    	   poisonX = -20;
    	   poisonY = -20;
      }
-     void show()
+     public void show()
      {
      	if(mousePressed && mouseButton == LEFT)
  	   {
@@ -114,3 +131,12 @@
      	ellipse(poisonX,poisonY,20,20);	
      }
  } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
